adc68602633ece639a644786426cada1f2b84025
ocean <2282724674@qq.com>
2025-07-12 22:18:47 +0800

    add support for RimWorld 1.6

--- RimTrans.Trans/Program.cs
+++ RimTrans.Trans/Program.cs
@@ -280,7 +280,7 @@ namespace RimTrans.Trans
         {
             var loadFoldersPath = Path.Combine(modPath, "LoadFolders.xml");
             var dirNames = new[] {
-                "", "Common", "1.0", "1.1", "1.2", "1.3", "1.4", "1.5"
+                "", "Common", "1.0", "1.1", "1.2", "1.3", "1.4", "1.5", "1.6"
             };
             if (File.Exists(loadFoldersPath))
             {
